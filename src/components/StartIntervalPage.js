c68719343a4f646483dc7edefb380ac61af8e47c
James Hale <james.hale.1890@googlemail.com>
2019-02-28 16:55:35 +0000

    Added tests with mocks for intervals using `jest.useFakeTimers()` Added buttons for restarting interval step and restarting whole interval routine

--- src/components/StartIntervalPage.js
+++ src/components/StartIntervalPage.js
@@ -9,30 +9,35 @@ export class StartIntervalPage extends React.Component {
     super(props)
     this.state = {
       currentStepIndex: 0,
-      currentStepName: '',
-      currentTimeRemaining: '',
+      currentStepName: undefined,
+      currentTimeRemaining: undefined,
       isRunning: false
     }
     this.interval = undefined
   }
 
-  initialise = () => {
-    const currentStepIndex = this.state.currentStepIndex
+  initialise = (stepIndex) => {
+    // const currentStepIndex = this.state.currentStepIndex
     this.setState(() => ({
-      currentStepName: this.props.interval.steps[currentStepIndex].name,
-      currentTimeRemaining: this.props.interval.steps[currentStepIndex].duration
+      currentStepName: this.props.interval.steps[stepIndex].name,
+      currentTimeRemaining: this.props.interval.steps[stepIndex].duration
     }))
   }
 
   runTimer = () => {
+    if (!this.state.isRunning) {
+      return
+    }
+
     if (this.state.currentTimeRemaining == 0) {
       if (this.state.currentStepIndex < (this.props.interval.steps.length - 1)) {
         this.setState((prevState) => ({
           currentStepIndex: (prevState.currentStepIndex + 1)
         }))
-        this.initialise()
+        this.initialise(this.state.currentStepIndex)
       } else {
         window.clearInterval(this.interval)
+        this.setState(() => ({ isRunning: false }))
       }
 
     } else {
@@ -45,7 +50,9 @@ export class StartIntervalPage extends React.Component {
   handleStart = (e) => {
     e.preventDefault()
 
-    this.initialise()
+    if (this.state.currentTimeRemaining == undefined) {
+      this.initialise(0)
+    }
     this.setState(({ isRunning: true }))
     this.interval = setInterval(this.runTimer, 1000)
 
@@ -55,6 +62,25 @@ export class StartIntervalPage extends React.Component {
     e.preventDefault()
 
     window.clearInterval(this.interval)
+    this.setState(() => ({ isRunning: false }))
+  }
+
+  handleRestartStep = (e) => {
+    e.preventDefault()
+
+    this.initialise(this.state.currentStepIndex)
+    this.setState(({ isRunning: true }))
+    this.interval = setInterval(this.runTimer, 1000)
+
+  }
+
+  handleRestartWholeInterval = (e) => {
+    e.preventDefault()
+
+    this.setState(() => ({ currentStepIndex: 0 }))
+    this.initialise(0)
+    this.setState(({ isRunning: true }))
+    this.interval = setInterval(this.runTimer, 1000)
 
   }
 
@@ -66,6 +92,12 @@ export class StartIntervalPage extends React.Component {
         <h1>{this.state.currentTimeRemaining}</h1>
         <button onClick={this.handleStart}>Start</button>
         <button onClick={this.handleStop}>Stop</button>
+        {!this.state.isRunning && this.state.currentStepName != undefined &&
+          <>
+            <button onClick={this.handleRestartStep}>Restart Step</button>
+            <button onClick={this.handleRestartWholeInterval}>Restart Interval</button>
+          </>
+        }
       </div>
     )
   }
--- src/tests/components/StartIntervalPage.test.js
+++ src/tests/components/StartIntervalPage.test.js
@@ -6,21 +6,207 @@ import { intervals } from '../fixtures/intervals'
 let wrapper
 
 beforeEach(() => {
+  // Use these two for mocking the intervals
+  jest.clearAllTimers()
+  jest.useFakeTimers()
+
   wrapper = shallow(<StartIntervalPage interval={intervals[0]} />)
 })
 
-test('StartIntervalPage should render correclty', () => {
+afterEach(() => {
+  jest.clearAllTimers()
+})
+
+test('StartIntervalPage should render correctly', () => {
   expect(wrapper).toMatchSnapshot()
 })
 
-//TODO - interval should start when button is clicked
 
-//TODO - interval should change state current step and time remaining when launched
+test('Interval should start when button is clicked', () => {
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
+  expect(setInterval).toHaveBeenCalledTimes(1)
+  expect(setInterval).toHaveBeenCalledWith(expect.any(Function), 1000)
+})
 
-//TODO - interval should clear when stop button is clicked
 
-//TODO - interval should resume when start button is clicked after being stopped
+test('Interval should change state current step and time remaining when launched', () => {
+  //start interval
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
 
-//TODO - restart step button should re-initialise step
+  expect(wrapper.state().currentStepIndex).toEqual(0)
+  expect(wrapper.state().currentStepName).toEqual(intervals[0].steps[0].name)
+  expect(wrapper.state().currentTimeRemaining).toEqual(intervals[0].steps[0].duration)
+  expect(wrapper).toMatchSnapshot()
 
-//TODO - restart interval should re-initialise interval
+})
+
+
+test('Interval should clear when stop button is clicked', () => {
+  //start interval
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
+  //stop interval
+  wrapper.find('button').at(1).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  expect(clearInterval).toHaveBeenCalledTimes(1)
+})
+
+
+test('Time remaining of interval step should reduce by 1 second for each second the interval runs for', () => {
+  //start interval
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  jest.advanceTimersByTime(1000)
+
+  expect(wrapper.state().currentStepIndex).toEqual(0)
+  expect(wrapper.state().currentStepName).toEqual(intervals[0].steps[0].name)
+  expect(wrapper.state().currentTimeRemaining).toEqual((intervals[0].steps[0].duration - 1))
+
+  expect(wrapper).toMatchSnapshot()
+})
+
+
+test('When the timer reaches zero, the next second should set the interval step name and the time remaining to that of the next step', () => {
+  //start interval
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  jest.advanceTimersByTime(1000 * (intervals[0].steps[0].duration + 1))
+
+  expect(wrapper.state().currentStepIndex).toEqual(1)
+  expect(wrapper.state().currentStepName).toEqual(intervals[0].steps[1].name)
+  expect(wrapper.state().currentTimeRemaining).toEqual((intervals[0].steps[1].duration))
+  expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Should stop the interval after all of the steps have completed', () => {
+  //start interval
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  for (let i = 0; i < intervals[0].steps.length; i++) {
+    jest.advanceTimersByTime(1000 * (intervals[0].steps[i].duration + 1))
+  }
+
+  const lastStepIndex = intervals[0].steps.length - 1
+  expect(wrapper.state().currentStepIndex).toEqual(lastStepIndex)
+  expect(wrapper.state().currentStepName).toEqual(intervals[0].steps[lastStepIndex].name)
+  expect(wrapper.state().currentTimeRemaining).toEqual(0)
+  expect(setInterval).toHaveBeenCalledTimes(1)
+  expect(clearInterval).toHaveBeenCalledTimes(1)
+  expect(wrapper).toMatchSnapshot()
+
+})
+
+test('Stopping the timer should stop the time remaining from decreasing', () => {
+  //start interval
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  jest.advanceTimersByTime(2000)
+
+  //stop interval
+  wrapper.find('button').at(1).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  jest.advanceTimersByTime(4000)
+
+  expect(wrapper.state().currentStepIndex).toEqual(0)
+  expect(wrapper.state().currentStepName).toEqual(intervals[0].steps[0].name)
+  expect(wrapper.state().currentTimeRemaining).toEqual((intervals[0].steps[0].duration - 2))
+
+  expect(wrapper).toMatchSnapshot()
+})
+
+
+test('interval should resume from current time remaining when start button is clicked after being stopped', () => {
+  //start interval
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  jest.advanceTimersByTime(2000)
+
+  //stop interval
+  wrapper.find('button').at(1).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  //resume interval
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  jest.advanceTimersByTime(2000)
+
+  expect(wrapper.state().currentStepIndex).toEqual(0)
+  expect(wrapper.state().currentStepName).toEqual(intervals[0].steps[0].name)
+  expect(wrapper.state().currentTimeRemaining).toEqual((intervals[0].steps[0].duration - 4))
+
+  expect(wrapper).toMatchSnapshot()
+
+})
+
+
+test('"Restart Step" button should reinitialise current step', () => {
+  //start interval
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  jest.advanceTimersByTime(25000)
+
+  //stop interval
+  wrapper.find('button').at(1).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  //re-start interval step
+  wrapper.find('button').at(2).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  expect(wrapper.state().currentStepIndex).toEqual(1)
+  expect(wrapper.state().currentStepName).toEqual(intervals[0].steps[1].name)
+  expect(wrapper.state().currentTimeRemaining).toEqual((intervals[0].steps[1].duration))
+  expect(wrapper).toMatchSnapshot()
+})
+
+
+test('"Restart Step" button should reinitialise current step', () => {
+  //start interval
+  wrapper.find('button').at(0).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  jest.advanceTimersByTime(25000)
+
+  //stop interval
+  wrapper.find('button').at(1).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  //re-start interval step
+  wrapper.find('button').at(3).simulate('click', {
+    preventDefault: () => { }
+  })
+
+  expect(wrapper.state().currentStepIndex).toEqual(0)
+  expect(wrapper.state().currentStepName).toEqual(intervals[0].steps[0].name)
+  expect(wrapper.state().currentTimeRemaining).toEqual((intervals[0].steps[0].duration))
+  expect(wrapper).toMatchSnapshot()
+})
